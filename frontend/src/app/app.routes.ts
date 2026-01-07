392969c8584fb04495ba22e3929c563c81100589
Giovanna Tavares <109823597+giovannatvrs@users.noreply.github.com>
2026-01-07 16:09:42 -0300

    Página de login

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { SignUp } from './pages/sign-up/sign-up';
+
+export const routes: Routes = [{
+    path: 'login',
+    component: Login
+},
+{
+    path: 'sign-up',
+    component: SignUp
+}];
