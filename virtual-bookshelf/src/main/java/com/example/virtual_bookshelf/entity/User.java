286b68e321808cdd0225a6e17239cb51b0ffe9a9
giovanna <giovannatvrs@outlook.com>
2026-01-09 02:33:25 -0300

    responsividade na tela de login

--- virtual-bookshelf/src/main/java/com/example/virtual_bookshelf/entity/User.java
+++ virtual-bookshelf/src/main/java/com/example/virtual_bookshelf/entity/User.java
@@ -1,30 +1,52 @@
 package com.example.virtual_bookshelf.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.security.auth.Subject;
+import java.security.Principal;
+import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name="User")
 @Entity
-public class User {
+@EntityListeners(AuditingEntityListener.class)
+public class User implements UserDetails, Principal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="user_id")
     private long id;
 
-    @Column(name="user_name")
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+
+    @Column(name="enabled")
+    private boolean enabled;
+
+    @Column(name="account_locked")
+    private boolean accountLocked;
+
+    @Column(name="username", unique = true)
     private String name;
 
-    @Column(name="user_email")
+    @Column(name="user_email", unique = true)
     private String email;
 
     @Column(name="user_password")
@@ -39,5 +61,43 @@ public class User {
     @OneToMany(mappedBy = "user")
     private Set<Bookshelf> bookshelves;
 
+    @Override
+    public String getName(){
+        return email;
+    }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !accountLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    @Override
+    public boolean implies(Subject subject) {
+        return Principal.super.implies(subject);
+    }
 }
