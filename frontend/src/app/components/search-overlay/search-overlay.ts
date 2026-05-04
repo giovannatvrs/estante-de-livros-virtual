1735b9f04e60ba14e3ff02f76565733b571dcaf1
giovanna <giovannatvrs@outlook.com>
2026-05-04 14:52:06 -0300

    continuação na busca de livros

--- frontend/src/app/components/search-overlay/search-overlay.ts
+++ frontend/src/app/components/search-overlay/search-overlay.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { MatDivider } from '@angular/material/divider';
 @Component({
   selector: 'app-search-overlay',
-  imports: [],
+  imports: [MatDivider],
   templateUrl: './search-overlay.html',
   styleUrl: './search-overlay.scss',
 })
